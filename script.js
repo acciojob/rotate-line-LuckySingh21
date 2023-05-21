d3c635bd1683ed7a81e1268bd7eef3ebbacf010c
LuckySingh21 <128302021+LuckySingh21@users.noreply.github.com>
2023-05-21 14:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let temp = document.getElementById("line");
+
+function rotate() {
+	let new = document.createElement("div");
+	new.className = "line";
+	temp.append(new);
+}
+rotate();
+	
+	
+}
